307bfd6eab0802966dc461fcafc2324f1d648a4c
michaelstsnow <michael.st.snow@gmail.com>
2019-03-29 04:08:57 +0000

    Done.

---
+++ lib/game.rb
@@ -0,0 +1,37 @@
+class Game
+  attr_accessor(:board,:player_1,:player_2)
+
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+    @player_1=player_1
+    @player_2=player_2
+    @board=board
+  end
+
+  WIN_COMBINATIONS = [
+  [0,1,2], #Top row
+  [3,4,5], #Middle row
+  [6,7,8], #Bottom row
+  [0,3,6], #Left column
+  [1,4,7], #Middle column
+  [2,5,8], #Right column
+  [0,4,8], #Diagnol One
+  [6,4,2], #Diagnol Two
+  ]
+
+  def player_1=(p1)
+    @player_1=p1
+  end
+
+  def player_2=(p2)
+    @player_2=p2
+  end
+
+  def current_player
+    if @board.turn_count.even? || @board.turn_count.zero?
+      return @player_1
+    else
+      return @player_2
+    end
+  end
+
+end
